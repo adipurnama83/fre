ec7208d4717889bec681d43af8e4b100b0ac29a7
adipurnama83 <adi.purnama1983@gmail.com>
2021-09-05 16:59:36 +0700

    Add files via upload

--- cfg.php
+++ cfg.php
@@ -1,5 +1,18 @@
-<?
-//PERHATIKAN CARA ADMIN MENGISI DATA
+<?php
 
-$cookie = "xxxxx";
-$user = "xxxxx";
+// Looked Tutorial Video And Setup User & cookie ....
+// join Now Telegram And Enjoy Any Earning Script Update
+// Do You Want Hacking Information Service To Call Me
+// Every Day YouR Cookie Is Change Script Is Not Working Looked Tutorial .
+
+
+
+
+$user = "xxx";
+$cookie = "xxxx";
+
+
+
+
+
+      //❤️❤️❤️
